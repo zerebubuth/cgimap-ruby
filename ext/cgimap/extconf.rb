405f328c4f31f9cb2e6afa58152b40ddedd312a7
Matt Amos <zerebubuth@gmail.com>
2018-09-22 17:16:15 +0100

    Added define to CXXFLAGS, needed on my system. Don't use auto in have_library check, as that doesn't seem to respect CXXFLAGS - but we can explicitly state the type, so it doesn't matter.

--- ext/cgimap/extconf.rb
+++ ext/cgimap/extconf.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'mkmf-rice'
 
-$CXXFLAGS << ' -std=c++11'
+$CXXFLAGS << ' -std=c++11 -DHAVE_CXX11'
 $CPPFLAGS << ' -I/usr/include/libxml2'
 
 dir_config 'cgimap'
@@ -11,7 +11,7 @@ abort "missing pqxx" unless have_library('pqxx', 'pqxx::connection c("")', 'pqxx
 abort "missing boost::program_options" unless have_library('boost_program_options', 'boost::program_options::options_description options("")', 'boost/program_options.hpp')
 abort "missing boost::regex" unless have_library('boost_regex', 'boost::regex re("")', 'boost/regex.hpp')
 abort "missing boost::locale" unless have_library('boost_locale', 'std::string demo = boost::locale::to_upper("demo")', 'boost/locale.hpp')
-abort "missing boost::system" unless have_library('boost_system', 'auto ec = boost::system::errc::make_error_code(boost::system::errc::not_supported)','boost/system/error_code.hpp')
+abort "missing boost::system" unless have_library('boost_system', 'boost::system::error_code ec = boost::system::errc::make_error_code(boost::system::errc::not_supported)','boost/system/error_code.hpp')
 abort "missing boost::date_time" unless have_library('boost_date_time', 'boost::gregorian::date d(2002,boost::gregorian::Jan,10)','boost/date_time/gregorian/gregorian.hpp')
 abort "missing libxml2" unless have_library('xml2', 'xmlTextWriterPtr ptr = xmlNewTextWriter(NULL)', 'libxml/xmlwriter.h')
 abort "missing zlib" unless have_library('z', 'z_stream strm;inflateInit(&strm)', 'zlib.h')
